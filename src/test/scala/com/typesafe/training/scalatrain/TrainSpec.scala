7c560948bf0d38be5ce0208fdae859cf49c98548
Pallay Raunu <pallay.raunu@googlemail.com>
2015-05-13 10:20:23 +0100

    Adding departure time

--- src/test/scala/com/typesafe/training/scalatrain/TrainSpec.scala
+++ src/test/scala/com/typesafe/training/scalatrain/TrainSpec.scala
@@ -48,7 +48,7 @@ class TrainSpec extends WordSpec with Matchers {
 
   "departureTimes" should {
     "the correct pair of departure time and station" in {
-      ice724.departureTimes shouldEqual(Vector(("08:50", munich)), ("10:00",nuremberg), ("12:10", frankfurt), ("13:39",cologne))
+      ice724.departureTimes shouldEqual Vector((Time(8,50), munich), (Time(10),nuremberg), (Time(12,10), frankfurt))
     }
   }
 
