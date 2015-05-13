c687b006c4dec91c4891adbb89809c045a63bd26
Pallay Raunu <pallay.raunu@googlemail.com>
2015-05-13 11:00:57 +0100

    Adding alternative to departure times for hop

--- src/test/scala/com/typesafe/training/scalatrain/HopSpec.scala
+++ src/test/scala/com/typesafe/training/scalatrain/HopSpec.scala
@@ -14,5 +14,12 @@ class HopSpec extends WordSpec with Matchers {
     }
   }
 
-
+  "departureAndArrivalTime" should {
+    "throw an exception for empty train schedule" in {
+      an[IllegalArgumentException] should be thrownBy Hop(ice724.stations.head, ice724.stations.head, new Train(InterCityExpress(724), Vector()))
+    }
+    "throw an exception for a train with stations that aren't backtoback" in {
+      an[IllegalArgumentException] should be thrownBy Hop(ice726.stations.head, ice726.stations.last, ice726)
+    }
+  }
 }
