6952396ef01493c5120480a89cfe4ab520467a38
Duc Hoang <duch@cakesolutions.net>
2015-05-13 10:58:27 +0100

    First version depart/arrive

--- src/main/scala/com/typesafe/training/scalatrain/Hop.scala
+++ src/main/scala/com/typesafe/training/scalatrain/Hop.scala
@@ -4,7 +4,7 @@ case class Hop(from: Station, to: Station, train: Train) {
   require(train.backToBackStations.nonEmpty)
   require(train.backToBackStations.contains((from, to)))
 
-  def calculateDepArrTime: (Time, Time) = {
+  def departureAndArrivalTime: (Time, Time) = {
     val fromSchedule = train.schedule.find(timeStation => timeStation._2 == from)
     val toSchedule = train.schedule.find(timeStation => timeStation._2 == to)
     assert(fromSchedule.nonEmpty)
