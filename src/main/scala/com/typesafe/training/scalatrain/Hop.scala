0fbe9940c941b96f3c979a2e59e8a79497af7770
Pallay Raunu <pallay.raunu@googlemail.com>
2015-05-13 10:45:49 +0100

    Adding hop validation

--- src/main/scala/com/typesafe/training/scalatrain/Hop.scala
+++ src/main/scala/com/typesafe/training/scalatrain/Hop.scala
@@ -1,3 +1,6 @@
 package com.typesafe.training.scalatrain
 
-case class Hop(from: Station, to: Station, train: Train)
+case class Hop(from: Station, to: Station, train: Train) {
+  require(train.backToBackStations.nonEmpty)
+  require(train.backToBackStations.contains((from, to)))
+}
--- src/test/scala/com/typesafe/training/scalatrain/HopSpec.scala
+++ src/test/scala/com/typesafe/training/scalatrain/HopSpec.scala
@@ -1,6 +1,18 @@
 package com.typesafe.training.scalatrain
 
+import TestData._
 import org.scalatest.{Matchers, WordSpec}
 
-class TimeSpec extends WordSpec with Matchers {
+class HopSpec extends WordSpec with Matchers {
+
+  "new Hope" should {
+    "throw an exception for empty train schedule" in {
+      an[IllegalArgumentException] should be thrownBy Hop(ice724.stations.head, ice724.stations.head, new Train(InterCityExpress(724), Vector()))
+    }
+    "throw an exception for a train with stations that aren't backtoback" in {
+      an[IllegalArgumentException] should be thrownBy Hop(ice726.stations.head, ice726.stations.last, ice726)
+    }
+  }
+
+
 }
